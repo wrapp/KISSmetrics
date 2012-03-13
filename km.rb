ea9e31b372dec9795fd2bcfc3092818589422d72
Per Almquist <pelle@wrapp.com>
2012-03-13 18:04:32 +0100

    Removed files that is unused by the Python implementation

--- km.rb
+++
@@ -1,5 +0,0 @@
-# The ruby client has moved to https://github.com/kissmetrics/km
-#
-# Please note that there's a new binary km_send to do the sending which this lib did previously.
-#
-# You can also install the gem by running `gem install km`.
